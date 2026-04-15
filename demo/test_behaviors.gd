51b9ef3287cdb6a12fabe288d805a2a41d183073
Thomas Key <thomas.key@periodyc.com>
2026-04-15 14:24:41 -0700

    Setup gdformat in IDE
    Setup gdformat in IDE (gdscript-formatter ate native script symbols and
    mucked up the file)
    Tested formatting with gdformat and found it acceptable

--- demo/test_behaviors.gd
+++ demo/test_behaviors.gd
@@ -1,14 +1,14 @@
 extends Node
 
-@export
-var blackboard_a: HydrogenBlackboard = null
+@export var blackboard_a: HydrogenBlackboard = null
 
-@export
-var blackboard_b: HydrogenBlackboard = null
+@export var blackboard_b: HydrogenBlackboard = null
+
 
 func try_running_tests():
-	if (BehaviorServer.has_method("run_tests")):
+	if BehaviorServer.has_method("run_tests"):
 		BehaviorServer.run_tests()
+
 
 func try_exporting_type_infos():
 	var dict: Dictionary = BehaviorServer.blackboard_export_type_infos()
@@ -16,12 +16,14 @@ func try_exporting_type_infos():
 	var json_str: String = JSON.stringify(dict)
 	print(json_str)
 
+
 func try_blackboard_create_and_destruct():
 	var bb_rid: RID = BehaviorServer.blackboard_create()
 	BehaviorServer.blackboard_set_int(bb_rid, "MyInt", 42)
 	print("My Int: ", BehaviorServer.blackboard_get_int(bb_rid, "MyInt"))
 
 	BehaviorServer.free_rid(bb_rid)
+
 
 func try_blackboard_create_and_destruct_by_ref():
 	var bb: HydrogenBlackboard = HydrogenBlackboard.new()
@@ -34,12 +36,13 @@ func try_testing_existing_blackboards():
 	if not blackboard_a or not blackboard_b:
 		return
 
-	if (blackboard_a.parent == blackboard_b):
+	if blackboard_a.parent == blackboard_b:
 		print(blackboard_a, "'s parent is ", blackboard_b)
-	else: if (blackboard_b.parent == blackboard_a):
-		print(blackboard_b, "'s parent is ", blackboard_a)
 	else:
-		print("neither a nor b have each other as parents")
+		if blackboard_b.parent == blackboard_a:
+			print(blackboard_b, "'s parent is ", blackboard_a)
+		else:
+			print("neither a nor b have each other as parents")
 
 	var entries_a: Dictionary = blackboard_a.export_entries()
 	print("A's contents: \n", entries_a)
@@ -48,7 +51,7 @@ func try_testing_existing_blackboards():
 	print("B's contents: \n", entries_b)
 
 	var my_color_test: Color = blackboard_a.get_color(&"favorite_color")
-	if (my_color_test != null):
+	if my_color_test != null:
 		var my_color: Color = my_color_test
 		print("My Color: ", my_color)
 
